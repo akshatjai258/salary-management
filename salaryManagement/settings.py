e036e15a9c901d0d8c299c3227643b0109c00196
Shabari S Nair <shabarisnair123@gmail.com>
2020-10-31 23:21:01 +0530

    added media folders

--- salaryManagement/settings.py
+++ salaryManagement/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -129,3 +131,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+
+MEDIA_ROOT= os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
