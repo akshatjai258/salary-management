5092234cabdb31bed4adfc5e79bbceb53a380791
Shabari S Nair <shabarisnair123@gmail.com>
2020-11-24 10:00:02 +0530

    registered LeaveStatus model

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Leave,Employee,Contact,hrProfile
+from .models import Leave,Employee,Contact,hrProfile,LeaveStatus
 
 # Register your models here.
 admin.site.register(Leave)
 admin.site.register(Employee)
 admin.site.register(Contact)
 admin.site.register(hrProfile)
+admin.site.register(LeaveStatus)
